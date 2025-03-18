11573db521e8295e61a33ea3090d4337c9053017
pablloedu <pablloedu@gmail.com>
2025-03-17 22:29:01 -0300

    programa base ja estilizado

--- estoque/models.py
+++ estoque/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=200)
+    categoria = models.CharField(max_length=100, blank=True, null=True)
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    quantidade = models.IntegerField(default=0)
+    fornecedor = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+    def estoque_baixo(self):
+        return self.quantidade < 5  # Define um alerta de estoque baixo
--- estoque/views.py
+++ estoque/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Produto
+from .forms import ProdutoForm
 
-# Create your views here.
+def lista_produtos(request):
+    query = request.GET.get('q')  
+    min_qtd = request.GET.get('min_qtd')  
+    max_qtd = request.GET.get('max_qtd')  
+    min_preco = request.GET.get('min_preco')  
+    max_preco = request.GET.get('max_preco')  
+
+    produtos = Produto.objects.all()
+    LIMITE_ESTOQUE_BAIXO = 5  # Definição do limite
+
+    if query:
+        produtos = produtos.filter(nome__icontains=query)
+    if min_qtd:
+        produtos = produtos.filter(quantidade__gte=min_qtd)
+    if max_qtd:
+        produtos = produtos.filter(quantidade__lte=max_qtd)
+    if min_preco:
+        produtos = produtos.filter(preco__gte=min_preco)
+    if max_preco:
+        produtos = produtos.filter(preco__lte=max_preco)
+
+    return render(request, 'estoque/lista_produtos.html', {
+        'produtos': produtos,
+        'query': query,
+        'min_qtd': min_qtd,
+        'max_qtd': max_qtd,
+        'min_preco': min_preco,
+        'max_preco': max_preco,
+        'limite_estoque_baixo': LIMITE_ESTOQUE_BAIXO,  # Passando para o template
+    })
+
+def adicionar_produto(request):
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm()
+    return render(request, 'estoque/adicionar_produto.html', {'form': form})
+
+def editar_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_produtos')
+    else:
+        form = ProdutoForm(instance=produto)
+    return render(request, 'estoque/editar_produto.html', {'form': form, 'produto': produto})
+
+def excluir_produto(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        produto.delete()
+        return redirect('lista_produtos')
+    return render(request, 'estoque/excluir_produto.html', {'produto': produto})
